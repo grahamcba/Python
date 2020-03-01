4ad31bdf80dd1f44f2b33b7bddbf999dd58f4fb1
Graham <grahamcba>
2020-03-01 17:23:42 +1100

    First commit of MelbourneReddit.

--- MelbourneReddit/melbournereddit.py
+++ MelbourneReddit/melbournereddit.py
@@ -1,9 +1,8 @@
 import praw
-import json
-import requests
 import tweepy
 import time
 import datetime
+import logging
 
 def strip_title(title):
     if len(title) < 115:
@@ -14,11 +13,12 @@ def strip_title(title):
 def tweet_creator(subreddit_info):
     post_dict = {}
     post_ids = []
-    print "[bot] Getting posts from Reddit"
+    print "[STATUS] Getting posts from Reddit"
+    logging.info('[STATUS] Getting posts from Reddit')
     for submission in subreddit_info.get_new(limit=1):
         post_dict[strip_title(submission.title)] = submission.url
         post_ids.append(submission.id)
-    print "[bot] Generating short link using goo.gl"
+    print "[STATUS] Generating short link using goo.gl"
     mini_post_dict = {}
     for post in post_dict:
         post_title = post
@@ -29,19 +29,19 @@ def tweet_creator(subreddit_info):
 
 def setup_connection_reddit(subreddit):
     time = datetime.datetime.now().time()
-    print str(time) + "\n[bot] setting up connection with Reddit"
-    r = praw.Reddit('reddit sydney twitter bot '
+    print str(time) + "\n[STATUS] setting up connection with Reddit"
+    r = praw.Reddit('reddit melbourne twitter STATUS '
                     'monitoring %s' % (subreddit))
     subreddit = r.get_subreddit(subreddit)
     return subreddit
 
 def shorten(url):
-    headers = {'content-type': 'application/json'}
-    payload = {"longUrl": url}
-    url = "https://www.googleapis.com/urlshortener/v1/url"
-    r = requests.post(url, data=json.dumps(payload), headers=headers)
-    link = json.loads(r.text)['id']
-    return link
+    # headers = {'content-type': 'application/json'}
+    # payload = {"longUrl": url}
+    # url = "https://www.googleapis.com/urlshortener/v1/url"
+    # r = requests.post(url, data=json.dumps(payload), headers=headers)
+    # link = json.loads(r.text)['id']
+    return url
 
 def duplicate_check(id):
     found = 0
@@ -56,22 +56,23 @@ def add_id_to_file(id):
         file.write(str(id) + "\n")
 
 def main():
+    logging.info('[STATUS] Script beginning')
     restart = True
     while restart == True:
         try:
             while True:
                 restart = False
-                subreddit = setup_connection_reddit('sydney')
+                subreddit = setup_connection_reddit('melbourne')
                 post_dict, post_ids = tweet_creator(subreddit)
                 tweeter(post_dict, post_ids)
         except Exception, e:
             restart = True
             print e
-            print "[bot] Exception caught - sleeping 900 secs"
+            print "[STATUS] Exception caught - sleeping 900 secs"
             time.sleep(900)
 
 def tweeter(post_dict, post_ids):
-    f = open('SydneyReddit.txt')
+    f = open('MelbourneReddit.txt')
     lines = f.readlines()
     f.close()
     access_token = lines[0].strip()
@@ -84,18 +85,17 @@ def tweeter(post_dict, post_ids):
     for post, post_id in zip(post_dict, post_ids):
         found = duplicate_check(post)
         if found == 0:
-            print "[bot] Posting this link on twitter"
-            print post.encode('ascii', 'ignore') + " " + post_dict[post] + " #Sydney"
-            api.update_status(post.encode('ascii', 'ignore') + " " + post_dict[post] + " #Sydney")
+            print "[STATUS] Posting this link on twitter"
+            print post.encode('ascii', 'ignore') + " " + post_dict[post] + " #Melbourne"
+            api.update_status(post.encode('ascii', 'ignore') + " " + post_dict[post] + " #Melbourne")
 
-            print "[bot] Adding to file"
+            print "[STATUS] Adding to file"
             add_id_to_file(post.encode('ascii', 'ignore'))
-            print "[bot] Sleeping for 120 secs"
-            time.sleep(120)
+            print "[STATUS] Sleeping for 30 secs"
+            time.sleep(30)
         else:
-            print "[bot] Already posted"
-            print "[bot] Sleeping for 120 secs"
-            time.sleep(120)
+            print "[STATUS] Already posted"
+            print "[STATUS] Sleeping for 30 secs"
+            time.sleep(30)
 
-if __name__ == '__main__':
-    main()
+main()
